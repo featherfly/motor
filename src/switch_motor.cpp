3da21c30416c5a36d24e37bc3234b79783d259c9
zhongji <featherfly@foxmail.com>
2020-11-04 17:11:26 +0800

    修复switch_motor的问题

--- src/switch_motor.cpp
+++ src/switch_motor.cpp
@@ -5,9 +5,7 @@ SwitchMotor::SwitchMotor() {}
 
 SwitchMotor::SwitchMotor(Motor *_motor, Switch* _start, Switch* _end)
 {
-    motor = _motor;
-    start = _start;
-    end = _end;
+    setup(_motor, _start, _end);
 }
 
 void SwitchMotor::setup(Motor *_motor, Switch* _start, Switch* _end)
@@ -15,6 +13,7 @@ void SwitchMotor::setup(Motor *_motor, Switch* _start, Switch* _end)
     motor = _motor;
     start = _start;
     end = _end;
+    Log.notice(F("setup motor start end" CR));
 }
 void SwitchMotor::setMotor(Motor *_motor)
 {
@@ -30,23 +29,29 @@ void SwitchMotor::setSwitch(Switch *_end)
 }
 void SwitchMotor::forward()
 {
-    if (end->is_off())
+    Log.notice(F("switch_motor.forward end->is_on() %T" CR), end->is_on());
+    if (end->is_on())
     {
+        Log.notice(F("switch_motor.forward motor->forward()" CR));
         motor->forward();
     }
     while(end->is_on())
     {
     }
+    Log.notice(F("switch_motor.forward motor->stop(), end->is_on() %T" CR), end->is_on());
     motor->stop();
 }
 void SwitchMotor::backward()
 {
-    if (start->is_off())
+    Log.notice(F("switch_motor.backward start->is_on() %T" CR), start->is_on());
+    if (start->is_on())
     {
+        Log.notice(F("switch_motor.backward motor->backward()" CR));
         motor->backward();
     }
     while (start->is_on())
     {
     }
+    Log.notice(F("switch_motor.backward motor->stop(), start->is_on() %T" CR), start->is_on());
     motor->stop();
 }
